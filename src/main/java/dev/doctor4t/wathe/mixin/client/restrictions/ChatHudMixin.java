f115c7348002a0b4bf38c25bcff66fa4a97e1b68
Matthew-MBG <61323301+Matthew-MBG@users.noreply.github.com>
2026-04-09 18:10:40 +0200

    Modify chat render condition to include proximity mod

--- src/main/java/dev/doctor4t/wathe/mixin/client/restrictions/ChatHudMixin.java
+++ src/main/java/dev/doctor4t/wathe/mixin/client/restrictions/ChatHudMixin.java
@@ -3,6 +3,7 @@ package dev.doctor4t.wathe.mixin.client.restrictions;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import dev.doctor4t.wathe.client.WatheClient;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.ChatHud;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,7 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 public class ChatHudMixin {
     @WrapMethod(method = "render")
     public void wathe$disableChatRender(DrawContext context, int currentTick, int mouseX, int mouseY, boolean focused, Operation<Void> original) {
-        if (!WatheClient.isPlayerAliveAndInSurvival()) {
+        if (!WatheClient.isPlayerAliveAndInSurvival() || FabricLoader.getInstance().isModLoaded("proximity")) {
             original.call(context, currentTick, mouseX, mouseY, focused);
         }
     }
